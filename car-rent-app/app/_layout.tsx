f73444ee49d6e81e54dbebd769dbc89c3be62805
Maga10900 <maga168123@gmail.com>
2026-01-28 21:11:59 +0400

    TASK

--- car-rent-app/app/_layout.tsx
+++ car-rent-app/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 import "../global.css";
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return <Stack >
+    <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack.Screen name="home" options={{ headerShown: false }} />
+  </Stack>;
 }
--- car-rent-app/app/index.tsx
+++ car-rent-app/app/index.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
-
+import { Image, ImageBackground } from "expo-image";
+import { useRouter } from "expo-router";
+import { Platform, Pressable, Text, useWindowDimensions, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
+  const w = useWindowDimensions().width;
+  const h = useWindowDimensions().height;
+  const router = useRouter();
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <ImageBackground
+        source={require("../assets/images/First.jpg")}
+        style={{ width: "100%", height: "100%" }}>
+        <View className="flex-1 justify-between mb-2">
+          <Text className="text-white text-[48px] w-[317px] text-center mt-[108px]">Find and rent car in easy steps.</Text>
+          <Pressable className="bg-[#FF5C00] w-[317px] h-[56px] rounded-[16px] justify-center ml-[36px]" onPress={() => router.push("/home")}>
+            <Text className="text-white text-[18px] font-bold text-center">Let's Go</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
